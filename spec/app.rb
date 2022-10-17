874f393734f4efab753120d97ea453c4f0de87b1
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-17 12:34:33 +0300

    lab complete

---
+++ spec/app.rb
@@ -0,0 +1,10 @@
+# app.rb
+# print "Hello World!"
+# puts ("Hello World!")
+# p [1,4,5]
+# puts [1,4,5].inspect
+# pp [{id:1, hello:"World"},{id:2, hello:"Ruby"}, {id:3, hello:"Moon"},{id:4, hello:"Learner"}]
+ 
+puts "Hello World!"
+print"Pass this test, please."
+p [1, 2, 3]
